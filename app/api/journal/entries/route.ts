a7291efeb4f9aff0a8c3927a60629d7591e4149c
Terel <terel.phillips23@gmail.com>
2023-08-13 19:04:10 -0400

    -Implemented Apollo for fetching query

--- app/api/journal/entries/route.ts
+++ app/api/journal/entries/route.ts
@@ -1,8 +1,38 @@
 import { NextResponse } from 'next/server';
-import { getEntries } from '../../../../services/index';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
+const client = new ApolloClient({
+  uri: process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT as string,
+  cache: new InMemoryCache(),
+});
 export async function GET(req: Request) {
-  const entries = await getEntries();
+  const { data } = await client.query({
+    query: gql`
+      query Entries {
+        entriesConnection {
+          edges {
+            node {
+              title
+              id
+              slug
+              createdAt
+              excerpt
+              publishedAt
+              content {
+                markdown
+              }
+              featuredImage {
+                url
+              }
+              subtittle
+            }
+          }
+        }
+      }
+    `,
+  });
+
+  const entries = data.entriesConnection.edges;
 
   return NextResponse.json({
     message: 'Hello from /api/test',
--- app/api/properties/route.ts
+++ app/api/properties/route.ts
@@ -1,9 +1,47 @@
-import { getProperties } from '@/services';
 import { NextResponse } from 'next/server';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT as string,
+  cache: new InMemoryCache(),
+});
 
 export async function GET(req: Request) {
-  const properties = await getProperties();
+  const { data } = await client.query({
+    query: gql`
+      query Properties {
+        propertiesConnection {
+          edges {
+            node {
+              id
+              propertyName
+              publishedAt
+              slug
+              description {
+                text
+              }
+              features {
+                text
+              }
+              amenities {
+                text
+              }
+              images {
+                id
+                url
+                height
+                width
+              }
+              bathrooms
+              bedrooms
+            }
+          }
+        }
+      }
+    `,
+  });
 
+  const properties = data.propertiesConnection.edges;
   return NextResponse.json({
     message: 'Hello from /api/properties',
     properties,
