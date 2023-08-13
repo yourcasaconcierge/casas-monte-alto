551bc6558afe80f05e5b59489b4ee716efa0270e
Terel <terel.phillips23@gmail.com>
2023-08-13 19:13:36 -0400

    -commented out services

--- services/index.ts
+++ services/index.ts
@@ -1,77 +1,77 @@
-import { Entry } from '@/types/EntryTypes';
-import { Property } from '@/types/PropertyTypes';
-import { request, gql } from 'graphql-request';
+// import { Entry } from '@/types/EntryTypes';
+// import { Property } from '@/types/PropertyTypes';
+// import { request, gql } from 'graphql-request';
 
-const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT as string;
+// const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT as string;
 
-export const getEntries = async (): Promise<Entry[]> => {
-  const query = gql`
-    query Entries {
-      entriesConnection {
-        edges {
-          node {
-            title
-            id
-            slug
-            createdAt
-            excerpt
-            publishedAt
-            content {
-              markdown
-            }
-            featuredImage {
-              url
-            }
-            subtittle
-          }
-        }
-      }
-    }
-  `;
+// export const getEntries = async (): Promise<Entry[]> => {
+//   const query = gql`
+//     query Entries {
+//       entriesConnection {
+//         edges {
+//           node {
+//             title
+//             id
+//             slug
+//             createdAt
+//             excerpt
+//             publishedAt
+//             content {
+//               markdown
+//             }
+//             featuredImage {
+//               url
+//             }
+//             subtittle
+//           }
+//         }
+//       }
+//     }
+//   `;
 
-  const result: { entriesConnection: { edges: Entry[] } } = await request(
-    graphqlAPI,
-    query
-  );
-  return result.entriesConnection.edges;
-};
+//   const result: { entriesConnection: { edges: Entry[] } } = await request(
+//     graphqlAPI,
+//     query
+//   );
+//   return result.entriesConnection.edges;
+// };
 
-export const getProperties = async (): Promise<Property[]> => {
-  const query = gql`
-    query Properties {
-      propertiesConnection {
-        edges {
-          node {
-            id
-            propertyName
-            publishedAt
-            slug
-            description {
-              text
-            }
-            features {
-              text
-            }
-            amenities {
-              text
-            }
-            images {
-              id
-              url
-              height
-              width
-            }
-            bathrooms
-            bedrooms
-          }
-        }
-      }
-    }
-  `;
+// export const getProperties = async (): Promise<Property[]> => {
+//   const query = gql`
+//     query Properties {
+//       propertiesConnection {
+//         edges {
+//           node {
+//             id
+//             propertyName
+//             publishedAt
+//             slug
+//             description {
+//               text
+//             }
+//             features {
+//               text
+//             }
+//             amenities {
+//               text
+//             }
+//             images {
+//               id
+//               url
+//               height
+//               width
+//             }
+//             bathrooms
+//             bedrooms
+//           }
+//         }
+//       }
+//     }
+//   `;
 
-  const result: { propertiesConnection: { edges: Property[] } } = await request(
-    graphqlAPI,
-    query
-  );
-  return result.propertiesConnection.edges;
-};
+//   const result: { propertiesConnection: { edges: Property[] } } = await request(
+//     graphqlAPI,
+//     query
+//   );
+//   return result.propertiesConnection.edges;
+// };
