eb8027118e6f440fd906d0a9c31482860720d001
Terel <terel.phillips23@gmail.com>
2023-08-28 16:15:50 -0400

    -Style chanes to footer and header

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -10,8 +10,8 @@ import LogoSvg from './LogoSvg';
 const Footer = () => {
   const { foot } = useContext(LanguageContext);
   return (
-    <footer className="layout pb-5 max-sm:text-[0.6rem]">
-      <div className="py-14 border-t-2 border-secondary">
+    <footer className="layout pb-10 lg:pb-5 justify-end max-sm:text-[0.6rem]">
+      <div className="py-8 border-t-2 border-secondary">
         <Link
           href="https://www.instagram.com/casasmontealto/"
           className="text-base group"
@@ -23,14 +23,19 @@ const Footer = () => {
       <Image
         src="https://images.unsplash.com/photo-1668585418249-f87c0f926583?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
         alt="villa"
-        className="bg-secondary w-full h-[300px] lg:h-[600px] object-cover"
+        className="bg-secondary w-full h-[300px] lg:h-[500px] object-cover"
         width={1170}
         height={600}
       />
       <LogoSvg />
       <div className="flex justify-between">
         <p>{foot?.line1}</p>
-        <Link href="/">{foot?.line2}</Link>
+        <Link
+          href="https://www.linkedin.com/in/terel-phillips/"
+          target="_blank"
+        >
+          {foot?.line2}
+        </Link>
       </div>
     </footer>
   );
--- app/components/sections/Header.tsx
+++ app/components/sections/Header.tsx
@@ -9,13 +9,13 @@ import { LanguageContext } from '@/context/LanguageContext';
 const Header = () => {
   const { head } = useContext(LanguageContext);
   return (
-    <section className="border-b-2 border-secondary lg:min-h-screen max-lg:pb-14">
+    <section className="border-b-2 border-secondary lg:min-h-screen max-lg:pb-8">
       <LogoSvg hidden />
       <SmallLogoSvg />
       <Image
         src="https://images.unsplash.com/photo-1576013551627-0cc20b96c2a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
         alt="villa"
-        className="bg-secondary w-full h-[300px] lg:h-[600px] object-cover"
+        className="bg-secondary w-full h-[300px] lg:h-[500px] object-cover"
         width={1170}
         height={600}
       />
@@ -27,7 +27,3 @@ const Header = () => {
 };
 
 export default Header;
-
-// pb-14 lg:pb-32
-
-// lg:my-32
