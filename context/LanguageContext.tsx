a393ba5d207d719a3bde7f8d742fa5fe4b59cb7b
Terel <terel.phillips23@gmail.com>
2023-08-28 18:49:25 -0400

    -test

--- context/LanguageContext.tsx
+++ context/LanguageContext.tsx
@@ -68,10 +68,13 @@ export const LanguageContext = createContext<LanguageContextProps>({
 });
 
 export const LanguageProvider = ({ children }: any) => {
-  const languageStorage = localStorage.getItem('language');
-  const languageNavigator = navigator?.language.toLowerCase().startsWith('es')
-    ? 'spanish'
-    : 'english';
+  const languageStorage =
+    typeof window !== 'undefined' && localStorage.getItem('language');
+  const languageNavigator =
+    typeof window !== 'undefined' &&
+    navigator?.language.toLowerCase().startsWith('es')
+      ? 'spanish'
+      : 'english';
   const [language, setLanguage] = useState(
     languageStorage || languageNavigator
   );
@@ -80,7 +83,6 @@ export const LanguageProvider = ({ children }: any) => {
   const [cont, setCont] = useState(contact);
   const [foot, setFoot] = useState(footer);
   const [about, setAbout] = useState(aboutPage);
-  const [isClient, setIsClient] = useState(false);
 
   const toggleLanguage = () => {
     if (language === 'english') {
@@ -93,26 +95,20 @@ export const LanguageProvider = ({ children }: any) => {
   };
 
   useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  useEffect(() => {
-    if (isClient) {
-      if (language === 'english') {
-        setNav(navigation);
-        setHead(header);
-        setCont(contact);
-        setFoot(footer);
-        setAbout(aboutPage);
-      } else {
-        setNav(navS);
-        setHead(headS);
-        setCont(contS);
-        setFoot(footS);
-        setAbout(aboutPageS);
-      }
+    if (language === 'english') {
+      setNav(navigation);
+      setHead(header);
+      setCont(contact);
+      setFoot(footer);
+      setAbout(aboutPage);
+    } else {
+      setNav(navS);
+      setHead(headS);
+      setCont(contS);
+      setFoot(footS);
+      setAbout(aboutPageS);
     }
-  }, [isClient, language]);
+  }, [language]);
 
   const value = {
     language,
