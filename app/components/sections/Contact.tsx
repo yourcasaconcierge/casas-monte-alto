09848252271f244f5cbdd5b167e7d61a9e247313
Terel <terel.phillips23@gmail.com>
2023-08-30 14:34:46 -0400

    -Made contact button to a link -Now lead to contact page

--- app/components/sections/Contact.tsx
+++ app/components/sections/Contact.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { LanguageContext } from '@/context/LanguageContext';
+import Link from 'next/link';
 import { useContext } from 'react';
 
 const Contact = () => {
@@ -8,9 +9,12 @@ const Contact = () => {
   return (
     <section className="capitalize text-center h-screen w-full flex flex-col items-center justify-center gap-10 text-lg lg:text-3xl">
       <h4>{cont?.landing.heading}</h4>
-      <button className="capitalize bg-secondary text-primary p-3">
+      <Link
+        href="/contact"
+        className="capitalize bg-secondary text-primary p-3"
+      >
         {cont?.landing.button}
-      </button>
+      </Link>
     </section>
   );
 };
