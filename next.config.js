e6d5fbae3f15118991ab166788162cb7882a49d8
trendistack <terel.phillips23@gmail.com>
2025-06-20 15:08:52 -0400

    -updated hygraph image domain

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'images.unsplash.com',
-      'plus.unsplash.com',
-      'media.graphassets.com',
-    ],
+    domains: ['images.unsplash.com', 'plus.unsplash.com', '**.graphassets.com'],
   },
 };
 
